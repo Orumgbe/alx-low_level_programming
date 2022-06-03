66150ea0a2a921ae6e7aa4dbd2d2f39986ae5f03
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-02 18:24:51 -0700

    printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**Main - Holds all code
+ * Decription: Outputs text
+ * Return: returns zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

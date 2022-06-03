e0c9abbabae265349bbc8e3f2282f647848026c5
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-02 18:32:29 -0700

    printf command if it pleases betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*Main - Holds all code*/
-/*Description: Outputs text*/
-/*Return: returns zero*/
+/**
+ * Main - Holds all code
+ * Description: Outputs text
+ * Return: returns zero
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

5fa7112158cc6882caec53a181b24f35ae1bc102
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-03 15:09:46 -0700

    If statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Contains source code
+ * Description: Determines whether stored number + or -
+ * Return: returns zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+	/*Compares if number greater than zero*/
+	if (n > 0)
+		printf(" is positive\n");
+	/*Compares if number is same as zero*/
+	if (n == 0)
+		printf(" is zero\n");
+	/*Compares if number less than zero*/	
+	if (n < 0)
+		printf(" is negative\n");
+	return (0);
+}

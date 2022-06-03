837b831ad6ab56ff02c4b6f20035437297c08a83
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-03 16:22:17 -0700

    Ascii alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Holds source code
+ * Description: Prints alphabet with ascii value
+ * Return: returns zero
+ */
+int main(void)
+{
+	int c;
+
+	c = 97;
+	/* Ascii a-z == 97 to 123 */
+	while (c < 123)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}

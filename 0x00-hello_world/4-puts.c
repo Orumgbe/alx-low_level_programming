60ab42042936e032aa49412996aa3085e76f1673
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-02 17:46:37 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main(void)/*Using puts to output text*/
+/**
+ * Using puts command to output text
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

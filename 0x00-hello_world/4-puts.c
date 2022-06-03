20306bc24618f678ba19a3d9195d78d842feaf5d
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-02 18:12:27 -0700

    puts command if it pleases Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main function - Embodies code
+ *main - Embodies code
+ *Description: Outputs text
+ *Return: Returns zero
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/*Returns zero*/
 	return (0);
 }

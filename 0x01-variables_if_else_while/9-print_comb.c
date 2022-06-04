19c8114f6c0726fa6d53d3db6911dc1bebe71b89
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-03 18:39:35 -0700

    Perspiration? I think not.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,19 +3,20 @@
 /**
  * main - Holds Code
  * Description: Ascii 48 to 57 is decimal 0 to 9
- * 	Working with a nested for loop
+ * Working with a nested for loop
  * Return: retuns 0
  */
 int main(void)
 {
 	short x;
 
-	for (x = 48; x < 58; x++)
+	for (x = 48; x < 57; x++)
 	{
-			putchar(x);
-			putchar(',');
-			putchar(' ');
+		putchar(x);
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
+	x = 57;
+	putchar(x);
 	return (0);
 }

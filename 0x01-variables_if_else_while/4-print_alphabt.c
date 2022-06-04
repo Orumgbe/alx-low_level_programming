0487cce003391d91be260afd50277dcdf384034a
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-03 17:16:22 -0700

    Alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Holds source code
+ * Description: Prints alphabet with ascii value
+ * Return: returns zero
+ */
+int main(void)
+{
+	int l, u;
+
+	l = 97;
+	/* Ascii a-z == 97 to 122 */
+	while (l < 123)
+	{
+		if (!(l = 101 || l = 113))
+		{
+			putchar(l);
+			l++;
+		}
+		else
+		{
+			l++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

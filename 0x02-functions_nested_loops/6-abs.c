80ba06c07512955e9be50e4db6e207f3d99f9a7a
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-07 15:44:36 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "math.h"
+/**
+ * _abs - check the code
+ * @ : integer argument
+ * Description: Computes absolute value
+ * Return: returns absolute value
+ */
+int _abs(int n)
+{
+	int m;
+
+	m = abs(n);
+	return (m);
+}

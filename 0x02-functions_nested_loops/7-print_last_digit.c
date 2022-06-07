8c7e61c396c92c3e51a6e0e21d4ae072a5fced27
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-07 16:02:50 -0700

    Modulus operator

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+/**
+ * print_last_digit - check the code
+ * @l: Last digit
+ * Description: Prints last digit of a number
+ * Return: returns 0
+ */
+int print_last_digit(int l)
+{
+	int r;
+
+	r = l % 10;
+	return (r);
+}

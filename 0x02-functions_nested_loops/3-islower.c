17d24d9142411575daa84fff2869984902efe9a6
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-07 13:54:29 -0700

    Check for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - check the code
+ * @c: Character to print
+ * Description:
+ * Return: On success, returns 1
+ * On error, 0 is returned
+ */
+int _islower(int c)
+{
+	char i;
+
+	i = tolower(c);
+	if (i == c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

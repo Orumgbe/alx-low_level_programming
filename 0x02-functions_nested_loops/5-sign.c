3cbe125de2bc860a406093dc66b7bd5fbe1cc8ef
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-07 15:23:05 -0700

    Positive or negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,20 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(45);
 		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
-		_putchar('1');
-		return ("-1");
+		int i = -1;
+
+		_putchar(45);
+		_putchar(49);
+		return (i);
 	}
 }

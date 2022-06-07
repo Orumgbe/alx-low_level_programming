334aac765428c54b79cff2c6160ab19fc2b6a3fb
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-07 16:53:04 -0700

    Time count

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,15 @@ void jack_bauer(void)
 	int h;
 	int m;
 
-	for (h = 0; h < 24; h++ )
+	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar(h + 48);
-			_putchar(h + 48);
+			_putchar(h / 10 + 48);
+			_putchar(h % 10 + 48);
 			_putchar(':');
-			_putchar(m + 48);
-			_putchar(m + 48);
+			_putchar(m / 10 + 48);
+			_putchar(m % 10 + 48);
 			_putchar('\n');
 		}
 	}

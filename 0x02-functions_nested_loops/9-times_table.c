d12d001a2420fb808027cfb06fe77075ffd738e9
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-07 19:11:10 -0700

    Multiply to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,6 +22,7 @@ void times_table(void)
 				_putchar(val + 48);
 				_putchar(',');
 				_putchar(' ');
+				_putchar('\n');
 			}
 			else
 			{
@@ -29,8 +30,8 @@ void times_table(void)
 				_putchar(val % 10 + 48);
 				_putchar(',');
 				_putchar(' ');
-			}
 				_putchar('\n');
+			}
 		}
 	}
 	_putchar('\n');

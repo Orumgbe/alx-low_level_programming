dcdddd47c719127093f6d730fc8a72e31434b883
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-07 17:20:00 -0700

    Sums numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.c"
+/**
+ * add - check the code
+ * @i: first digit to sum
+ * @j: second digit to sum
+ * Description: Adds two numbers
+ * Return: returns sum;
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <math.h>
+/**
+ * time_table - check the code
+ *
+ * Description: Prints timestable from 0 to 9
+ * Return: returns nothing
+ */
+void times_table(void)
+{
+	int r;
+	int c;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			
+		}
+	}
+}

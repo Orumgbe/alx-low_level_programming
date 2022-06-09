457eb0b84e6a3621669663ece20428b163c51f57
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-09 15:56:29 -0700

    Fizz Buzz FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Contains the code
+ * Returns: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (((i % 3) == 0) && !(((i % 3) == 0) && ((i % 5) == 0)))
+		{
+			printf("Fizz");
+		}
+		else if (((i % 5) == 0) && !(((i % 3) == 0) && ((i % 5) == 0)))
+		{
+			printf("Buzz");
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf(i);
+		}
+		putchar(' ');
+	}
+	return (0);
+}

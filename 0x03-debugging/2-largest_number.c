9ea94046ccc72c9c132a2db0c32b5ff3bd763ffc
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-09 13:05:16 -0700

    Patch 1 and 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && a > c) || (a > b && b > c))
+	if ((a >= b && a >= c) || (a >= b && b >= c))
 	{
 		largest = a;
 	}
-	else if ((b > a && b > c) || (b > a && a > c))
+	else if ((b > a= && b >= c) || (b >= a && a >= c))
 	{
 		largest = b;
 	}
-	else if ((c > a && c > b) || (c > a && a > b))
+	else if ((c >= a && c >= b) || (c >= a && a >= b))
 	{
 		largest = c;
 	}

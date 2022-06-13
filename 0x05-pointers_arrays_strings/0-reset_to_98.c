585b3a19eac13a74a3fb3675e2572a1250a41bd8
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-13 11:59:13 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+ *reset_to_98 - function to update a pointer value
+ *@*n: pointer variable
+ *Return: voided
+ */
+void reset_to_98(int *n)
+{
+	int p;
+
+	p = 98;
+	n = &p;
+}

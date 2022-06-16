19e2bfaf8a07654e57e0525c8fd1f8893c20390c
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-16 14:00:46 -0700

    leet encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * leet - Encodes string
+ * @c: Character passed into function
+ * Return: Encoded string
+ */
+char *leet(char *c)
+{
+	int i, n, size;
+
+	i = 0;
+	size = 5;
+	char ARR[size] = {'A', 'E', 'O', 'T', 'L'};
+	char arr[size] = {'a', 'e', 'o', 't', 'l'};
+	char rep[size] = {'4', '3', '0', '7', '1'};
+
+	while (c[i] != '\0')
+	{
+		n = 0;
+		while (n < size)
+		{
+			if (c[i] == ARR[n] || c[i] = arr[n])
+			{
+				c[i] = rep[n];
+			}
+			n++;
+		}
+		i++;
+	}
+	return (c);
+}

f34417af1ce996b51c9636840ef5218751eaff7e
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-22 23:48:50 +0100

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - Returns value of a number to another number
+ * @x: base integer
+ * @y: exponent integer
+ * Return: Value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, (y - 1));
+	}
+}

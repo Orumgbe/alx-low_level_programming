e24507c000a18bbda1ad0ae7a02b2689aced1bbf
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-24 15:50:58 -0700

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: argument vector
  * Return: 0
  */
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	exit(EXIT_SUCCESS);

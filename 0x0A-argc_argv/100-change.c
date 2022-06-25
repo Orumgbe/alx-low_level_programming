27c24e7d0a95cf9b23ccfb8061c08a16e5b765fd
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-24 18:11:24 -0700

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,31 +9,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, val;
+	int i, val, result;
 	int unit[5] = {25, 10, 5, 2, 1}
 
-	val = 0;
-	if (atoi(argv[1]) < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	else if (argc != 2)
+	val = atoi(argv[1]);
+	result = 0;
+	if (argc != 2)
 	{
 		printf("%s\n", "Error");
 		return (1);
+	}
+	if (val < 0)
+	{
+		printf("0\n");
+		return (0);
 	}
 	else
 	{
 		for (i = 0; i < 5; i++)
 		{
-			while (atoi(argv[1]) >= unit[i])
+			while (val >= unit[i])
 			{
-				atoi(argv[1]) -= unit[i];
-				val++;
+				val -= unit[i];
+				result++;
 			}
 		}
-		printf("%d\n", val);
+		printf("%d\n", result);
 		return (0);
 	}
 }

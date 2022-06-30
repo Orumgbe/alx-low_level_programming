24ac19fc4c92fd81edadd2eed8d25e51ead89601
Orumgbe <orumgbeugbekile@gmail.com>
2022-06-30 13:46:43 -0700

    Empty functions

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strpbrk - Searches string for set of characters
+ * @s: string literal
+ * @accept: string literal
+ * Return: pointer to beginning of substring
+ * on error, NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return;
+}

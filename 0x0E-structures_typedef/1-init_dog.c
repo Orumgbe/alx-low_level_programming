7d049aa6ac78a9845eda674436d504bc21c4d8cd
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-04 16:59:33 -0700

    Initialize struct variables

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * init_dog - Initializes struct dog data
+ * @d: struct identifier
+ * @name: string literal
+ * @age: float parameter
+ * @owner: string literal
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

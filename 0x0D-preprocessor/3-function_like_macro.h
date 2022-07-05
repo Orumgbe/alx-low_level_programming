36545da6c6969e670969be927bbbd2ffeb9a6d37
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-04 18:01:02 -0700

    fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
+#ifndef 3-FUNCTION_LIKE_MACRO_
+#define 3-FUNCTION_LIKE_MACRO_
 #define ABS(x) (((x) < 0) ? -(x) : (x))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef 4-SUM_H
+#define 4-SUM_H
 #define SUM(x, y) ((x) + (y))
 #endif

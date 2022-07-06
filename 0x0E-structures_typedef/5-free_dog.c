6bfc78746b37d0b438767201f7cb45cc83eea595
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-06 07:21:59 -0700

    free malloc

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free allocated memory
+ * @d: struct pointer
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}

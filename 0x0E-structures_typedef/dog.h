cd734b7f9470b9c6bbd38e765bb3576b0e5d09f6
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-06 05:01:43 -0700

    Dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-/**
- * struct dog - Dog data
- * @name: first data
- * @age: second data
- * @owner: third data
- */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+typedef struct dog dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

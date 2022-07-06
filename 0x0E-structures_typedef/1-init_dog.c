8fd130b238b31c8131a547c08bff1a2659377c09
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-06 05:37:02 -0700

    Test

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,5 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog init = {&init, "Poppy", 3.5, "Bob"};
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }

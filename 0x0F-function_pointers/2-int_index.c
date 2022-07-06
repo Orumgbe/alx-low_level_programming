6010a371f9e367cd2f342d0c66fe7a513da3eee1
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-06 16:28:12 -0700

    Compares integers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,22 +9,20 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
+	i = 0;
+	if (array == NULL || cmp == NULL || size <= 0)
 	{
 		return (-1);
 	}
 	else
 	{
-		for(i = 0; i < size; i++)
+		while (i < size)
 		{
 			if (cmp(array[i]) != 0)
 			{
 				return (i);
 			}
 		}
-		else
-		{
-			return (-1);
-		}
+		return (-1);
 	}
 }

adffa3b9110a77b2c37432716220dc0843272165
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-06 18:16:29 -0700

    Command line calculator

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * get_op_func - function pointer to get operator
+ * @s: operation to be performed
+ * Return: pointer to function that corresponds to operator
+ * On error, NULL
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op == s)
+		{
+			return (ops[i].f);
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

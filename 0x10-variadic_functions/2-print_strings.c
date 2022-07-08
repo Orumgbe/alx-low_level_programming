30d61bec515ff6a7b66757fd2bcd5b872d3aa843
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-07 18:14:04 -0700

    string print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,11 +28,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			}
 			else
 			{
-				printf("%s", va_arg(all, char *));
+				printf("%s", str);
 			}
 			if (separator != NULL && i != n - 1)
 			{
-				printf("%c ", *separator);
+				printf("%s ", separator);
 			}
 		}
 		va_end(all);

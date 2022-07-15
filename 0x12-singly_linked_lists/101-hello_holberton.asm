61346d4d450022cb5aa23ea9a4fa9cc830ce468d
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-14 20:24:22 -0700

    still bugged

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,16 @@
+global _start
 section .text
-	global _start
 _start:
 	mov edx, len
 	mov ecx, msg
 	mov ebx, 1
-	mov eax, 4
+	mov eax, 0x4
 	int 0x80
 
-	mov eax, 1
+	mov eax, 0x1
+	mov ebx, 0
 	int 0x08
 
 section .data
-	text db "Hello, Holberton",10
-	len equ $ -text
+	msg db "Hello, Holberton",10
+	len equ $ -msg

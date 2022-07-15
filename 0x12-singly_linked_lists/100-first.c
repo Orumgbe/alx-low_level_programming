2acef2c022a2b920bca4ab0b00de4ff3488cf240
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-15 03:57:36 +0100

    attribute constructor - before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_first - function prototype name
+ * Return: return type void
+ */
+void print_first(void) __attribute__((constructor));
+/**
+ * print_first - Prints text before main
+ * Return: void
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

325d7042d14fef55c4dc36c5fdf577eac9cf9958
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-20 02:07:47 +0100

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,8 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	ptr = head;
-	count = 0;
-	while (ptr)
+	while (count <= index)
 	{
 		if (count == index)
 			return (ptr);

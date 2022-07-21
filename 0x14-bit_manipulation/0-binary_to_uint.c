9ef626126e620f097f28f0e1072e2703b42c2893
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-21 20:10:47 +0100

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: string of binary bits
+ * Return: the converted number
+ * on failure, return 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int bin, i;
+	unsigned int dec, rem, pval;
+
+	bin = 0;
+	for (i = 0; b[i] != '\0'; ++i)
+	{
+		if (b[i] < 48 || b[i] > 49)
+			return (0);
+		bin = bin * 10 + (b[i] - '0');
+	}
+	dec = 0;
+	pval = 1;
+	while (bin)
+	{
+		rem = bin % 10;
+		dec = dec + (rem * pval);
+		bin = bin / 10;
+		pval *= 2;
+	}
+	return (dec);
+}

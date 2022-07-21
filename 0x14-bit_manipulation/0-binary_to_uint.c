ff24bf527f4b28059c849f37f7d27a9a7cdaf118
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-21 20:29:23 +0100

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,12 +15,14 @@ unsigned int binary_to_uint(const char *b)
 	bin = 0;
 	for (i = 0; b[i] != '\0'; ++i)
 	{
-		if (b[i] != 48 || b[i] != 49)
+		if (b[i] < 48 || b[i] > 49)
 			return (0);
 		bin = bin * 10 + (b[i] - '0');
 	}
 	dec = 0;
 	pval = 1;
+	if (bin == 0)
+		return (0);
 	while (bin)
 	{
 		rem = bin % 10;

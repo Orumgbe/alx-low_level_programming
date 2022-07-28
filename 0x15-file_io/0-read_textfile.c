4d8a43f998625ffe161fc90c31d0949a32609557
Orumgbe <orumgbeugbekile@gmail.com>
2022-07-28 21:57:16 +0100

    read() system call

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <stddef.h>
+#include "main.h"
+/**
+ * read_textfile - reads and print textfile to POSIX stdout
+ * @filename: name of the file
+ * @letters: number of letters to be read
+ * Return: actual number of letters read and printed
+ * on failure, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, read_count, count;
+	char *text;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY, 0400);
+	if (fd == -1)
+		return (0);
+	text = malloc(sizeof(char) * letters);
+	read_count = read(fd, text, letters);
+	if (read_count == -1)
+	{
+		free(text);
+		return (0);
+	}
+	count = write(STDOUT_FILENO, text, read_count);
+	if (write == -1)
+	{
+		free(text);
+		return (0);
+	}
+	close(fd);
+	return (count);
+}

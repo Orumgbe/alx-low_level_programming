05386a376fbfe0710988c65bb0186cc43ef0d592
Orumgbe <orumgbeugbekile@gmail.com>
2022-09-07 16:27:34 -0700

    fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * sum_dlistint - sums data in linked list
+ * @head: pointer to head node
+ * Return: sum value
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	dlistint_t *ptr = NULL;

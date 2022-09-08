f6e115c21f5627726dcd6acf2e49ed7e1eab6e28
Orumgbe <orumgbeugbekile@gmail.com>
2022-09-08 07:04:12 -0700

    fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,6 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = ptr->next;
 		free(ptr);
+		ptr = NULL;
 		return (1);
 	}
 	while (ptr)
@@ -37,5 +38,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		ptr = ptr->next;
 		num++;
 	}
-	return (-1);
+	return (1);
 }

975d9ea5fb85c3b23eee5e0a871763c2403220e7
Orumgbe <orumgbeugbekile@gmail.com>
2022-10-02 07:50:32 -0700

    fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+unsigned int count(dlistint_t *h);
 /**
  * delete_dnodeint_at_index - deletes node at index of list
  * @head: double pointer to head
@@ -12,11 +13,17 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *ptr = NULL;
 	unsigned int num = 0;
+	unsigned int list_len = count(*head);
 
-	if (*head == NULL)
+	if (*head == NULL || list_len < index)
 		return (-1);
-	ptr = malloc(sizeof(dlistint_t));
 	ptr = *head;
+	if (ptr->next == NULL)
+	{
+		*head = NULL;
+		free(ptr);
+		return (1);
+	}
 	if (index == 0)
 	{
 		*head = ptr->next;
@@ -28,6 +35,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if (num == index)
 		{
+			if (ptr->next == NULL)
+			{
+				ptr->prev->next = NULL;
+				ptr = NULL;
+			}
 			ptr->prev->next = ptr->next;
 			ptr->next->prev = ptr->prev;
 			ptr->prev = NULL;
@@ -39,4 +51,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		num++;
 	}
 	return (1);
+}
+/**
+ * count - determine number of element in list
+ * @h: pointer to head node
+ * Return: number of elements in list
+ */
+unsigned int count(dlistint_t *h)
+{
+	dlistint_t *ptr;
+	unsigned int len;
+
+	ptr = h;
+	len = 0;
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		len++;
+	}
+	return (len);
 }

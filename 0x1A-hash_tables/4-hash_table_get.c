e8393f8acb4ef32a09e5f0146d8ae7f3ba9c2314
Orumgbe <orumgbeugbekile@gmail.com>
2023-02-28 07:07:38 -0800

    fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
+	if (key == NULL || ht == NULL || strlen(key) == 0)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 

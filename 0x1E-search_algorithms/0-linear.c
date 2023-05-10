4d78077a9bbff268ba9d42c1ba36ae6e93aa397e
Orumgbe <orumgbeugbekile@gmail.com>
2023-05-10 12:15:53 -0700

    task 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "search_algos.h"
+/**
+ * linear_search - linear search algorithm
+ * @array: pointer to first element of the array
+ * @size: size of the array
+ * @value: value to search for
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}

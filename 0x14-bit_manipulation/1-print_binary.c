3e6dc3cfac2fb61d3f3905449e23d3a069db10a8
Orumgbe <orumgbeugbekile@gmail.com>
2023-08-10 02:39:02 -0700

    fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,15 +7,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int num;
-	int i;
-
-	for (i = 31; i >= 0; i--)
-	{
-		num = n >> i;
-		if (num & 1)
-			putchar(49);
-		else
-			putchar(48);
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	if (n & 1)
+		_putchar(49);
+	else
+		_putchar(48);
 }
